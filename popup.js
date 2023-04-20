4cbd866ca919cf917f604ca6d4590b8256db5623
Stephen <stephen@mapsmith.net>
2023-04-20 19:56:29 -0400

    updates #2

---
+++ popup.js
@@ -0,0 +1,31 @@
+const datePicker = document.getElementById('custom-date-picker');
+const historyList = document.getElementById('custom-history-list');
+
+datePicker.addEventListener('change', (event) => {
+  const selectedDate = event.target.value;
+  filterHistoryEntriesByDate(selectedDate);
+});
+
+function filterHistoryEntriesByDate(selectedDate) {
+  chrome.history.search({
+    text: '',
+    startTime: new Date(selectedDate).getTime(),
+    endTime: new Date(selectedDate).getTime() + 86400000,
+    maxResults: 1000
+  }, (results) => {
+    historyList.innerHTML = '';
+
+    for (const entry of results) {
+      const entryElement = document.createElement('div');
+      entryElement.classList.add('entry');
+
+      const entryTitle = document.createElement('a');
+      entryTitle.href = entry.url;
+      entryTitle.target = '_blank';
+      entryTitle.textContent = entry.title || entry.url;
+      entryElement.appendChild(entryTitle);
+
+      historyList.appendChild(entryElement);
+    }
+  });
+}
