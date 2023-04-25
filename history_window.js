3af2f390c407c3f73e882d4ec1fb512caf53d206
Stephen <stephen@mapsmith.net>
2023-04-24 21:44:52 -0400

    #11

--- history_window.js
+++ history_window.js
@@ -20,36 +20,58 @@ function filterHistoryEntriesByDate(selectedDate) {
   }, (results) => {
     historyList.innerHTML = '';
 
-    // Sort the history items by lastVisitTime (in descending order)
-    results.sort((a, b) => b.lastVisitTime - a.lastVisitTime);
+    const processedResults = [];
 
-    for (const entry of results) {
-      const entryElement = document.createElement('div');
-      entryElement.classList.add('entry');
+    const processEntry = (entry) => {
+      return new Promise((resolve) => {
+        chrome.history.getVisits({ url: entry.url }, (visits) => {
+          visits.forEach((visit) => {
+            if (visit.visitTime >= new Date(selectedDate).getTime() &&
+              visit.visitTime <= new Date(selectedDate).getTime() + 86400000) {
+              const newEntry = Object.assign({}, entry);
+              newEntry.visitTime = visit.visitTime;
+              processedResults.push(newEntry);
+            }
+          });
+          resolve();
+        });
+      });
+    };
 
-      // Format the visited time
-      const visitedTime = new Date(entry.lastVisitTime);
-      const timeString = visitedTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    const promises = results.map(processEntry);
 
-      // Create a span element to display the visited time
-      const visitedTimeElement = document.createElement('span');
-      visitedTimeElement.textContent = `${timeString} - `;
-      entryElement.appendChild(visitedTimeElement);
+    Promise.all(promises).then(() => {
+      // Sort the history items by visitTime (in descending order)
+      processedResults.sort((a, b) => b.visitTime - a.visitTime);
 
-      // Create an img element for the favicon
-      const faviconElement = document.createElement('img');
-      faviconElement.src = `https://s2.googleusercontent.com/s2/favicons?domain=${entry.url}`;
-      faviconElement.alt = '';
-      faviconElement.classList.add('favicon');
-      entryElement.appendChild(faviconElement);
+      for (const entry of processedResults) {
+        const entryElement = document.createElement('div');
+        entryElement.classList.add('entry');
 
-      const entryTitle = document.createElement('a');
-      entryTitle.href = entry.url;
-      entryTitle.target = '_blank';
-      entryTitle.textContent = entry.title || entry.url;
-      entryElement.appendChild(entryTitle);
+        // Format the visited time for display purposes
+        const visitedTime = new Date(entry.visitTime);
+        const timeString = visitedTime.toLocaleTimeString('en-US', { hour12: true, hour: '2-digit', minute: '2-digit' });
 
-      historyList.appendChild(entryElement);
-    }
+        // Create a span element to display the visited time
+        const visitedTimeElement = document.createElement('span');
+        visitedTimeElement.textContent = `${timeString} - `;
+        entryElement.appendChild(visitedTimeElement);
+
+        // Create an img element for the favicon
+        const faviconElement = document.createElement('img');
+        faviconElement.src = `https://s2.googleusercontent.com/s2/favicons?domain=${entry.url}`;
+        faviconElement.alt = '';
+        faviconElement.classList.add('favicon');
+        entryElement.appendChild(faviconElement);
+
+        const entryTitle = document.createElement('a');
+        entryTitle.href = entry.url;
+        entryTitle.target = '_blank';
+        entryTitle.textContent = entry.title || entry.url;
+        entryElement.appendChild(entryTitle);
+
+        historyList.appendChild(entryElement);
+      }
+    });
   });
 }
